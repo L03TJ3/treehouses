27389020043a207b0e0155e20418bebe9299c651
lolopaluza <upsonmanoah@gmail.com>
2016-04-11 16:23:09 +0200

    merge changes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  recource :treehouses
-||||||| merged common ancestors
 
-=======
+  recource :treehouses
+
+
+
   resource :treehouses
->>>>>>> 0af2b748002092da7387c6b08aff530c04aaba01
 end
