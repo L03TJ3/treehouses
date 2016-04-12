df7d0d7763a66aed9d5cd44f33c1f837adfd1cc5
iulia0103 <iulia.leancu@gmail.com>
2016-04-12 16:50:39 +0200

    nearly awesome index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,3 @@ TreeHouse.create( { title: "Sunset Treehouse", description: "Entire home/2 Bedro
 TreeHouse.create( { title: "Midnight Treehouse", description: "Entire home/3 Bedroom/6 persons" , image: "img_7", price: "$1750 US per week ($255 per night)"      } )
 TreeHouse.create( { title: "Forest Rain Treehouse", description: "Entire home/1 Bedroom/2 persons" , image: "img_8", price: "$1500 US per week ($230 per night)"      } )
 TreeHouse.create( { title: "Splash Treehouse", description: "Entire home/1 Bedroom/2 persons" , image: "img_9", price: "$1450 US per week ($225 per night)"      } )
-TreeHouse.create( { title: "Sparrow Treehouse", description: "Entire home/2 Bedroom/4 persons" , image: "img_10", price: "$1600 US per week ($235 per night)"      } )
