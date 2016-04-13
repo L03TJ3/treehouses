633b28d942d90c30d7708fe4d769fca72edf5a2d
Laurence <laurenceweening@gmail.com>
2016-04-13 16:02:23 +0200

    Add controller Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "Hi. #{@user.name}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
