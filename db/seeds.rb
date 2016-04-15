4f2f3ae4e0a84f94c2704d77f0ce16e6a6706bd2
Laurence <laurenceweening@gmail.com>
2016-04-15 15:24:35 +0200

    Change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,56 @@
 User.create(email: "test@codaisseur.com", password: "foobar123")
+
 TreeHouse.create( {
   title: "Treehouse Lucia",
   description: "Entire home/2 Bedroom/4 persons",
   image: "img_1.jpg",
-  price: "$1600 US per week ($235 per night)"
+  price: 1600
 } )
-TreeHouse.create( { title: "Tranquil Resort Treehouse", description: "Entire home/1 Bedroom/2 persons", image: "img_2.jpg", price: "$1350 US per week  ($195 per night)"      } )
-TreeHouse.create( { title: "Tropical Treehouse", description: "Entire home/2 Bedroom/4 persons", image: "img_3.jpg", price: "$1650 US per week ($240 per night)"      } )
-TreeHouse.create( { title: "Maria Treehouse", description: "Entire home/2 Bedroom/4 persons", image: "img_4.jpg", price: "$1550 US per week ($235 per night)"      } )
-TreeHouse.create( { title: "White Lotus Treehouse", description: "Entire home/3 Bedroom/6 persons" , image: "img_5.jpg", price: "$1720 US per week ($252 per night)"      } )
-TreeHouse.create( { title: "Sunset Treehouse", description: "Entire home/2 Bedroom/4 persons" , image: "img_6.jpg", price: "$1600 US per week ($235 per night)"      } )
-TreeHouse.create( { title: "Midnight Treehouse", description: "Entire home/3 Bedroom/6 persons" , image: "img_7.jpg", price: "$1750 US per week ($255 per night)"      } )
-TreeHouse.create( { title: "Forest Rain Treehouse", description: "Entire home/1 Bedroom/2 persons" , image: "img_8.jpg", price: "$1500 US per week ($230 per night)"      } )
-TreeHouse.create( { title: "Splash Treehouse", description: "Entire home/1 Bedroom/2 persons" , image: "img_9.jpg", price: "$1450 US per week ($225 per night)"      } )
+TreeHouse.create( {
+  title: "Tranquil Resort Treehouse",
+  description: "Entire home/1 Bedroom/2 persons",
+  image: "img_2.jpg",
+  price: 1350
+} )
+TreeHouse.create( {
+  title: "Tropical Treehouse",
+  description: "Entire home/2 Bedroom/4 persons",
+  image: "img_3.jpg",
+  price: 1650
+} )
+TreeHouse.create( {
+  title: "Maria Treehouse",
+  description: "Entire home/2 Bedroom/4 persons",
+  image: "img_4.jpg",
+  price: 1550
+} )
+TreeHouse.create( {
+  title: "White Lotus Treehouse",
+  description: "Entire home/3 Bedroom/6 persons" ,
+  image: "img_5.jpg",
+  price: 1720
+} )
+TreeHouse.create( {
+  title: "Sunset Treehouse",
+  description: "Entire home/2 Bedroom/4 persons" ,
+  image: "img_6.jpg",
+  price: 1600
+} )
+TreeHouse.create( {
+  title: "Midnight Treehouse",
+  description: "Entire home/3 Bedroom/6 persons" ,
+  image: "img_7.jpg",
+  price: 1750
+} )
+TreeHouse.create( {
+  title: "Forest Rain Treehouse",
+  description: "Entire home/1 Bedroom/2 persons" ,
+  image: "img_8.jpg",
+  price: 1500
+} )
+TreeHouse.create( {
+  title: "Splash Treehouse",
+  description: "Entire home/1 Bedroom/2 persons" ,
+  image: "img_9.jpg", 
+  price: 1450
+} )
